b2cacaa968e9d2aadaaf127b135b9ffaea3e84e1
abhigarg543 <143832908+abhigarg543@users.noreply.github.com>
2026-06-20 10:57:12 +0530

    Delete Enterprise-Real-Time-Helpdesk-Ticket-Management-System-main directory

--- Enterprise-Real-Time-Helpdesk-Ticket-Management-System-main/backend/src/main/java/com/HelpDeskPro/Project/enums/RoleName.java
+++
@@ -1,7 +0,0 @@
-package com.HelpDeskPro.Project.enums;
-
-public enum RoleName {
-    ADMIN,
-    AGENT,
-    USER
-}
--- Enterprise-Real-Time-Helpdesk-Ticket-Management-System-main/backend/src/main/java/com/HelpDeskPro/Project/enums/TicketPriority.java
+++
@@ -1,9 +0,0 @@
-package com.HelpDeskPro.Project.enums;
-
-public enum TicketPriority {
-    LOW,
-    MEDIUM,
-    HIGH,
-    URGENT,
-    CRITICAL
-}
